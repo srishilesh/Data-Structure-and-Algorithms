f609664dfcf2582c3ae4948d4c932a5f6af79ee0
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-04-28 13:02:42 +0530

    Update Fast_food.java

--- Contests/Codechef/April Lunchtime 2019/Fast_food.java
+++ Contests/Codechef/April Lunchtime 2019/Fast_food.java
@@ -86,3 +86,59 @@ public class fastfood {
 	}
 
 }
+
+
+
+
+
+
+
+/*	CORRECT SOLUTION	*/
+
+/* 
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    
+		while(t-->0)
+	{
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		int b[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=sc.nextInt();
+		for(int i=0;i<n;i++)
+		b[i]=sc.nextInt();
+		for(int i=1;i<n;i++)
+		a[i]+=a[i-1];	
+		for(int i=n-2;i>=0;i--)
+		b[i]+=b[i+1];
+		long max=Integer.MIN_VALUE;
+		for(int i=0;i<n-1;i++)
+		{
+			if(a[i]+b[i+1]>max)
+				max=a[i]+b[i+1];
+		}
+		if(a[n-1]>max)
+		max=a[n-1];	
+		if(b[0]>max)
+		{
+			max=b[0];
+		}
+		System.out.println(max);
+		
+	}
+        
+	}
+}
+
+*/
