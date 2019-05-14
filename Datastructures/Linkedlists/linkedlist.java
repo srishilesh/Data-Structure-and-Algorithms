5240fcd2f5b151c63837646b5770d04d95d79b3e
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-05-15 01:43:32 +0530

    Added insertion

--- Datastructures/Linkedlists/linkedlist.java
+++ Datastructures/Linkedlists/linkedlist.java
@@ -34,13 +34,44 @@ class LinkedList
             
             llist.printList();
         }
+        public void push(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = head;
+            head = new_node;
+            
+            
+        }
+        public void insertAfter(Node prev_node, int new_data)
+        {
+            if(prev_node == null)
+            {System.out.println("Cannot be added");
+            return;
+            }
+            Node new_node = new Node(new_data);
+            new_node.next = prev_node.next;
+            prev_node.next = new_node;           
+            
+        }
+        public void insertEnd(int new_data)
+        {
+            if(head == null)
+            {
+                head = new Node(new_data);
+                return;
+            }
+            Node new_node = new Node(new_data);
+            new_node.next = null;
+            Node last = head;
+            while(last.next!= null)
+            last = last.next;
+            
+            last.next = new_node;
+            return;
+            
+            
+            
+            
+        }
         
     }
-
-            
-            
-            
-            
-        
- 
-
