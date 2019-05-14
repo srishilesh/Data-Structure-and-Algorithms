c2c4e1b58dcad8a8ac7201fa0212e07f63b17bc9
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-05-15 01:29:11 +0530

    Beginner code

--- Datastructures/Linkedlists/linkedlist.java
+++ Datastructures/Linkedlists/linkedlist.java
@@ -1,1 +1,46 @@
+class LinkedList
+{
+    Node head;
+    static class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;next = null;
+        }
+    }
+        public void printList()
+        {
+            Node n = head;
+            while(n!=null)
+            {
+                System.out.print(n.data+" ");
+                n = n.next;
+            }
+                        
+            
+        }
+        public static void main(String args[])
+        {
+            LinkedList llist = new LinkedList();
+            
+            llist.head = new Node(1);
+            Node second = new Node(2);
+            Node third = new Node(3);
+            
+            llist.head.next = second;
+            second.next = third;
+            
+            llist.printList();
+        }
+        
+    }
+
+            
+            
+            
+            
+        
+ 
 
