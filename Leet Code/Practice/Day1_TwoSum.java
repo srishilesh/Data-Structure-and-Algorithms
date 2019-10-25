dc0984f5c8bedf2ccd94621253af10bfef58c989
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-10-25 18:52:58 +0530

    With solution

--- Leet Code/Practice/Day1_TwoSum.java
+++ Leet Code/Practice/Day1_TwoSum.java
@@ -20,3 +20,35 @@ class Solution {
     }
 }
 
+
+// Solution 1
+// Two Pass Hash Table
+
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+            return new int[] { i, map.get(complement) };
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+
+// Solution 2
+// One Pass Hash Table
+
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
