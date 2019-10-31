afb29e9af4966531361d25a8b490cbbe87d2a90b
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-10-31 18:53:07 +0530

    Not working

--- Leet Code/Practice/Day2_add_two_numbers.java
+++ Leet Code/Practice/Day2_add_two_numbers.java
@@ -8,15 +8,17 @@
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode l3 = l1;
+        
         String s1="",s2="",sr1="",sr2="";
-       while(l1.next!=null && l2.next!=null)
+       while(l1!=null)
        {
            s1 = s1 + l1.val;
-           s2 = s2 + l2.val;
            l1 = l1.next;
+       }
+        while(l2!=null)
+       {
+           s2 = s2 + l2.val;
            l2 = l2.next;
-           
        }
         for(int i=s1.length()-1;i>=0;i--)
         {
@@ -26,15 +28,24 @@ class Solution {
         int one = Integer.parseInt(sr1);
         int two = Integer.parseInt(sr2);
         int three = one + two;
+        // System.out.println(one);
+        // System.out.println(two);
+        // System.out.println(three);
         String sr3 = three+"";
+        ListNode l3 = new ListNode(0) ;
+        l3.next = null;
         for(int i=sr3.length()-1;i>=0;i--)
         {
+            ListNode cur = new ListNode(Integer.parseInt(sr3.charAt(i)+""));
+            cur.next = null;
             if(l3.next!=null)
             {
-                l3.val = sr3.charAt(i);
+                l3.val = cur.val;
                 l3 = l3.next;
             }
+            System.out.println(cur.val);
         }
+        
         return l3;
        
     }
