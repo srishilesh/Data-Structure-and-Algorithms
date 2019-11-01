cb0396de32936941a7be8352a6e9232c8f4349aa
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-11-01 19:08:02 +0530

    Correct program

--- Leet Code/Practice/Day2_add_two_numbers.java
+++ Leet Code/Practice/Day2_add_two_numbers.java
@@ -59,3 +59,45 @@ class Solution {
     }
    
 }
+
+
+
+/*
+SOLUTION
+
+// *
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode(int x) { val = x; }
+//  * }
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+ListNode dummy = new ListNode(0);
+        int carry = 0;
+        ListNode p=l1;ListNode q = l2,cur = dummy;
+        // int x,y,sum;
+        while(p!=null || q!=null)
+        {
+            int x = (p!=null)?p.val:0;
+            int y = (q!=null)?q.val:0;
+            int sum = x + y + carry;
+            carry = sum/10;
+            ListNode newnode = new ListNode(sum%10);
+            cur.next = newnode;
+            cur = cur.next;
+            if(p!=null)p = p.next;
+            if(q!=null)q = q.next;
+            
+            
+            
+        }
+       
+        if(carry>0)
+            cur.next = new ListNode(carry);
+        return dummy.next;
+    }
+
+*/
