53d9615e746b4c49087544ba55c46f37c75beb05
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2020-01-12 12:16:32 +0530

    Update Day8_Maximum_Subarray.py

--- Leet Code/Practice/Day8_Maximum_Subarray.py
+++ Leet Code/Practice/Day8_Maximum_Subarray.py
@@ -7,11 +7,26 @@ class Solution:
         #     nums[i] = nums[i-1] + nums[i]
         # s = 0
         
-         for i in range(1,len(nums)):
-                nums[i] = max(nums[i], nums[i-1] + nums[i])
-         print(nums)
-         return max(nums)
-         
-# Kadane Algorithm
+         # for i in range(1,len(nums)):
+         #        nums[i] = max(nums[i], nums[i-1] + nums[i])
+         # print(nums)
+         # return max(nums)
+        
+# Kadane Algorithm        
+        # max_so_far = 0
+        # max_ending_here = 0
+        # for i in range(len(nums)):
+        #     max_ending_here = max_ending_here + nums[i]
+        #     if(max_ending_here<0):
+        #         max_ending_here = 0
+        #     if(max_so_far<max_ending_here):
+        #         max_so_far = max_ending_here
+        # return max_so_far
 
-# Dynamic Programming approach
+# Dynamic Programming approach        
+        max_so_far = nums[0]
+        curr_max = nums[0]
+        for i in range(1,len(nums)):
+            curr_max = max(nums[i],nums[i]+curr_max)
+            max_so_far = max(max_so_far,curr_max)
+        return max_so_far
